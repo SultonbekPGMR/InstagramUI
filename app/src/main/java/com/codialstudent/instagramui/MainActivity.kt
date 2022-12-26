3a88cd8915f8852cfa236600999cb10bd81a4a07
Sultonbek Tulanov <116726923+SultonbekPGMR@users.noreply.github.com>
2022-12-26 11:43:33 +0500

    Initial commit

---
+++ app/src/main/java/com/codialstudent/instagramui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.codialstudent.instagramui
+
+import android.graphics.Color
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import com.codialstudent.instagramui.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.statusBarColor = Color.WHITE
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+
+
+    }
+}
